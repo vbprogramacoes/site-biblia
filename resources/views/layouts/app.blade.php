fee3039273ad0694d2f8eb6214fc010b3eac2251
Viktor Mikhail Bitencourt <vbprogramacoes@gmail.com>
2022-06-10 14:18:07 -0300

    Ajusta icon site

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,7 +15,7 @@
     <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
 
     <!-- ICON IMAGE -->
-    <link href="{{ asset('img/punch.png') }}" rel="icon" type="image/png">
+    <link href="{{ asset('images/logo.png') }}" rel="icon" type="image/png">
 
     <!-- CSS -->
     <link href="{{ asset('css/style.css') }}" rel="stylesheet">
